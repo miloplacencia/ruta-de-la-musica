502e63442c4b552eea8c5992e50579da9175f3a9
Camilo Placencia <placenciameister@gmail.com>
2018-01-10 23:45:50 -0300

    heroku server test: enviando build y viendo si funciona sin get

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.use('/', express.static(path.join(__dirname, './build')));
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, './build/index.html')));
+app.use(express.static(path.join(__dirname, './build')));
+// app.route('/').get((req, res) => res.sendFile(path.join(__dirname, './build/index.html')));
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
