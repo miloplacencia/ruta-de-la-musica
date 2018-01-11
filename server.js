65787dd48c24a7f0fa0d1e005e0632dd6890aa01
Camilo Placencia <placenciameister@gmail.com>
2018-01-10 23:35:21 -0300

    testing server react

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const path = require('path');
 
 const PORT = process.env.PORT || 5000;
 const app = express();
-app
-  .use(express.static(path.join(__dirname, 'build')))
-  .get('/', (req, res) => res.sendFile(path.join(__dirname, './build/index.html')))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+app.use('/', express.static(path.join(__dirname, './build')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './build/index.html')));
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
